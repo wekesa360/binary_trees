a46c7e7de78ede93ff0b27bc8ec250c91edb3f66
Michael Wekesa <wekesa360@outlook.com>
2021-12-03 23:53:16 +0300

    Develpment: binary trees

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is aleaf
+ * @node: pointer to the node to check
+ * 
+ * Return: 1 if the node is a leaf, and 0 otherwise. If node is NULL, return 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node){
+
+    if(node != NULL && node->left == NULL && node->right == NULL)
+        return (1);
+
+    return 0;
+}
